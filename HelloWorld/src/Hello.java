b011080524f44bd773f83d584b64e0d72312f29d
Sanjaya Laknath Halwala <shalwala@Virtusa.com>
2019-10-07 01:43:33 +0530

    Initial project

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        int myFirstMNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstMNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println("myFirstMNumber : "+myTotal);
+        System.out.println("myLastOne : "+myLastOne);
+
+    }
+}
