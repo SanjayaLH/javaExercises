c214d65e30b8b36e3d327dc5b7af8bfa342efca4
Sanjaya Laknath Halwala <shalwala@Virtusa.com>
2019-10-11 23:16:06 +0530

    commit ChatAndBoolean

---
+++ ChatAndBoolean/src/com/sanja/Main.java
@@ -0,0 +1,17 @@
+package com.sanja;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyRightChar = '\u00A9';
+        System.out.println(myCopyRightChar);
+
+        boolean myTroueBooleanVal = true;
+        boolean myFalseBoolianVal = false;
+       boolean isCustomerOverTwenty = true;
+    }
+}
